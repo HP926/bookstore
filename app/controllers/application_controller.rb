794b8ab895892fd12e4111ce5282fd59c2d05099
hectorpina123@gmail.com <hectorpina123@gmail.com>
2015-06-09 14:15:02 -0600

    created books model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+rails generate model bookstore title:string author:string released_date:date
